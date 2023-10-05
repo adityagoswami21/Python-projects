8e7aa05117152cb94f5b39cd1a716223ba11630d
Chillpill9797 <adityagoswami193@gmail.com>
2023-10-05 23:54:08 +0530

    random color function added

--- spirograph.py
+++ spirograph.py
@@ -5,3 +5,12 @@ turtle.colormode(255)
 t = Turtle()
 t.shape("turtle")
 
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+
