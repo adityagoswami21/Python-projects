b7fdbb13244549e3dfe7c59ccc7ed79c29ffe313
Chillpill9797 <adityagoswami193@gmail.com>
2023-10-06 00:06:21 +0530

    drw spirograph function added, speed added

--- spirograph.py
+++ spirograph.py
@@ -14,3 +14,16 @@ def random_color():
     color = (r, g, b)
     return color
 
+
+t.speed("fastest")
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        t.color(random_color())
+        t.circle(100)
+        t.setheading(t.heading() + size_of_gap)
+
+
+draw_spirograph(5)
+screen.exitonclick()
