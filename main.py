5cff4fc7fd00d6a7960c667ebbf7e81133a8641d
Chillpill9797 <adityagoswami193@gmail.com>
2023-10-25 16:28:15 +0530

    initialising is race on param and appending all turtles to turtles list

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import turtle
 import random
 screen = Screen()
 
-
+is_race_on = False
 # screen.screensize(canvwidth=500, canvheight=400)
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Who will win the race?")
 
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
+turtles = []
 for player in range(0,6):
-    tim = Turtle(shape="turtle")
-    tim.color(colors[player])
-    tim.penup()
-    tim.goto(x=-230, y=y_positions[player])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[player])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[player])
+    turtles.append(new_turtle)
 
 
 
