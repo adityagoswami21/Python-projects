beeb736e5725f0fb6ce229cd356c130cde0607e9
Chillpill9797 <adityagoswami193@gmail.com>
2023-10-25 17:05:22 +0530

    modifying according to pep8 guidline

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import random
 screen = Screen()
 
 is_race_on = False
-# screen.screensize(canvwidth=500, canvheight=400)
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Who will win the race?")
 
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
 turtles = []
-for player in range(0,6):
+for player in range(0, 6):
     new_turtle = Turtle(shape="turtle")
     new_turtle.color(colors[player])
     new_turtle.penup()
@@ -22,7 +21,7 @@ if user_bet:
 
 while is_race_on:
     for turtle in turtles:
-        random_pace = random.randint(0,10)
+        random_pace = random.randint(0, 10)
         turtle.fd(random_pace)
         if turtle.xcor() > 230:
             is_race_on = False
@@ -32,13 +31,6 @@ while is_race_on:
             else:
                 print(f"You loose, the winner is {winning_color}")
             break
-
-
-
-
-
-
-
 
 
 screen.exitonclick()
