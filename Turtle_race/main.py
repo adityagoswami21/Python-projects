6c42a40c81eab5e6c241e1ef96aa97981f587610
Chillpill9797 <adityagoswami193@gmail.com>
2023-11-13 16:08:37 +0530

    resolving files not showing up problem

---
+++ Turtle_race/main.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+import random
+screen = Screen()
+
+is_race_on = False
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Who will win the race?")
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+turtles = []
+for player in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[player])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[player])
+    turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtles:
+        random_pace = random.randint(0, 10)
+        turtle.fd(random_pace)
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You win, the winner is {winning_color}")
+            else:
+                print(f"You loose, the winner is {winning_color}")
+            break
+
+
+screen.exitonclick()
