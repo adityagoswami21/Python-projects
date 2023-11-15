ed615123dd0a2253a8d9b176e9a91b1c0f14fca8
Chillpill9797 <adityagoswami193@gmail.com>
2023-11-15 11:58:42 +0530

    added two new projects

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -1,4 +1,5 @@
 import time
+import turtle
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
@@ -7,8 +8,11 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
+player = Player()
 game_is_on = True
+screen.listen()
+screen.onkey(fun=player.move_fd, key="Up")
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+screen.exitonclick()
