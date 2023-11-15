3ef6156ccbbd88bfea69c887519551dc22662a28
Chillpill9797 <adityagoswami193@gmail.com>
2023-11-15 21:24:02 +0530

    detect collision with car

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -8,9 +8,9 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+game_is_on = True
 player = Player()
 car_manager = CarManager()
-game_is_on = True
 screen.listen()
 screen.onkey(fun=player.move_fd, key="Up")
 while game_is_on:
@@ -18,3 +18,9 @@ while game_is_on:
     screen.update()
     car_manager.display_cars()
     car_manager.move_cars()
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+screen.exitonclick()
