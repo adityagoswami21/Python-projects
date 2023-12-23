c7b97946458e7f36b13e3b7588ab70a4784eac57
Chillpill9797 <adityagoswami193@gmail.com>
2023-12-23 21:14:14 +0530

    printing letters with actual names

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,5 +1,15 @@
 # opening the starting_letter.txt file
 with open("./Input/Letters/starting_letter.txt", "r") as letter:
     # modifying the letter in list form into a readable letter
-    f = "".join(letter)
-    print(f)
+    txt = "".join(letter)
+# opening invited_names.txt file
+with open("./Input/Names/invited_names.txt", "r") as n:
+    # reading each name
+    names = n.readlines()
+for name in names:
+    # replacing extra line from the letter
+    name = name.rstrip("\n")
+    # replacing [name] with actual names one by one
+    x = txt.replace("[name]", name)
+    # printing each letter
+    print(x)
