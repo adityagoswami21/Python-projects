ab84eee83273dc557bcf3ad205b5a4dd1f38c914
Chillpill9797 <adityagoswami193@gmail.com>
2023-12-25 14:57:18 +0530

    creating dictionary for the data

--- Squirell_census_DA/main.py
+++ Squirell_census_DA/main.py
@@ -4,3 +4,8 @@ data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_202312
 grey_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
 red_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
 black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels, red_squirrels, black_squirrels]
+}
