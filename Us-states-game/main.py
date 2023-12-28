3228bda09f0c486b5963c832fbbe5bda1f52c2dd
Chillpill9797 <adityagoswami193@gmail.com>
2023-12-28 22:27:45 +0530

    making the data of the state move to its location

--- Us-states-game/main.py
+++ Us-states-game/main.py
@@ -8,7 +8,13 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
+all_states = data.state.values
 answer_state = screen.textinput(title="Guess the State", prompt="What's the name of the state?").capitalize()
-if answer_state in data["state"].values:
-    state_data = data[data["state"] == answer_state]
-    print(state_data)
+if answer_state in all_states:
+    marker = turtle.Turtle()
+    marker.hideturtle()
+    marker.penup()
+    state_data = data[data.state == answer_state]
+    marker.goto(int(state_data.x), int(state_data.y))
+    marker.write(state_data.state)
+screen.exitonclick()
