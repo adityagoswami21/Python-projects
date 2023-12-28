c5889a92d68b0e280b413dafe864aec175e278a7
Chillpill9797 <adityagoswami193@gmail.com>
2023-12-28 21:50:40 +0530

    capitalize and checking guess from 50 states

--- Us-states-game/main.py
+++ Us-states-game/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,14 +7,8 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-# the below given code will give us the x and y coordinates of the map.
-
-
-def get_mouse_click_coor(x, y):
-    print(x, y)
-
-
-turtle.onscreenclick(get_mouse_click_coor)
-
-turtle.mainloop()  # keeping our screen open even when our code is finished running
-
+data = pandas.read_csv("50_states.csv")
+answer_state = screen.textinput(title="Guess the State", prompt="What's the name of the state?").capitalize()
+if answer_state in data["state"].values:
+    state_data = data[data["state"] == answer_state]
+    print(state_data)
