e845542e1f5efdf2d198a1962267182568c48d73
Chillpill9797 <adityagoswami193@gmail.com>
2023-12-28 20:05:16 +0530

    getting x and y coordinates on clicking the map

--- Us-states-game/main.py
+++ Us-states-game/main.py
@@ -6,5 +6,14 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+# the below given code will give us the x and y coordinates of the map.
 
-screen.exitonclick()
+
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+
+turtle.onscreenclick(get_mouse_click_coor)
+
+turtle.mainloop()  # keeping our screen open even when our code is finished running
+
