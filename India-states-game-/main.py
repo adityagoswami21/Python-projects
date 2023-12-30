9e02ed60ca8c918fa79552558cd1208670ec00f5
Chillpill9797 <adityagoswami193@gmail.com>
2023-12-30 20:43:40 +0530

    storing guessed states in a list

--- India-states-game-/main.py
+++ India-states-game-/main.py
@@ -1,8 +1,15 @@
 import turtle
+import pandas as pd
 screen = turtle.Screen()
 image = "India-state.gif"
 screen.setup(500, 500)
 screen.addshape(image)
 turtle.shape(image)
-
+data = pd.read_csv("28_states.csv")
+all_states = data.state.values
+guessed_state = []
+while len(guessed_state) < 28:
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/28 states", prompt="Guess the state").title()
+    if answer_state in all_states:
+        guessed_state.append(answer_state)
 screen.exitonclick()
