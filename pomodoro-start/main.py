c66e7b74e0b0c955932ea313ad2ae1c0e2135191
Chillpill9797 <adityagoswami193@gmail.com>
2024-01-05 15:53:25 +0530

    creating basic count-down mechanism

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -13,15 +13,25 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+
+def start_timer():
+    count_down(5)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+
+def count_down(count):
+    canvas.itemconfig(count_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
 
-start_button = Button(text="Start")
-start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset")
 reset_button.grid(column=2, row=2)
@@ -35,8 +45,10 @@ timer_text.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+count_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
+start_button = Button(text="Start", command=start_timer)
+start_button.grid(column=0, row=2)
 
 window.mainloop()
