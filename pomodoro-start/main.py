809b5698dc5f1e1e2e4d63ddb50121b529ea0010
Chillpill9797 <adityagoswami193@gmail.com>
2024-01-05 16:19:39 +0530

    adding 5 min timer

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,13 +16,15 @@ LONG_BREAK_MIN = 20
 
 
 def start_timer():
-    count_down(5)
+    count_down(5 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 
 def count_down(count):
-    canvas.itemconfig(count_text, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(count_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
