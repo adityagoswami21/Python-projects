4b11ce3bb2a0a2f210d603a03ecaa9c7d6d90935
Chillpill9797 <adityagoswami193@gmail.com>
2024-02-06 22:24:44 +0530

    sending mail on a particular day

--- Quotes-emailing-app/main.py
+++ Quotes-emailing-app/main.py
@@ -14,4 +14,8 @@ FRIEND = fnd
 with smtplib.SMTP("smtp.gmail.com") as connection:
     connection.starttls()
     connection.login(user=MY_EMAIL, password=PASSWORD)
-
+    if dt.datetime.today().weekday() == 0:
+        quotes = open("quotes.txt")
+        quote = quotes.readlines()
+        motivation = random.choice(quote)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=FRIEND, msg=f"Subject:Monday Motivation!\n\n{motivation}")
