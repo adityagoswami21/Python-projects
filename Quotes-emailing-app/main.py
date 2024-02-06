74a64d7f5ec5ea8fbca56ff36f7e7b1135ee76d0
Chillpill9797 <adityagoswami193@gmail.com>
2024-02-06 22:02:20 +0530

    reading mail and password from details.txt(.gitignore)

--- Quotes-emailing-app/main.py
+++ Quotes-emailing-app/main.py
@@ -1,8 +1,13 @@
 import smtplib
 import datetime as dt
 import random
-my_email = ""
-password = ""
+with open("details.txt", 'r') as mail:
+    info = mail.readlines()
+    user = info[0].rstrip("\n")
+    psw = info[1]
+
+my_email = f"{user}"
+password = f"{psw}"
 
 with smtplib.SMTP("smtp.gmail.com") as connection:
     connection.starttls()
