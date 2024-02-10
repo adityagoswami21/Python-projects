afc71a9b9e06b03d944aff43520c6770a0017b0a
Chillpill9797 <adityagoswami193@gmail.com>
2024-02-10 22:23:39 +0530

    checking for today

--- Birthday-wisher/main.py
+++ Birthday-wisher/main.py
@@ -1,8 +1,14 @@
-##################### Extra Hard Starting Project ######################
+import smtplib
+import datetime as dt
+import random
 
-# 1. Update the birthdays.csv
+import pandas as pd
 
+birthday = pd.read_csv("birthdays.csv")
 # 2. Check if today matches a birthday in the birthdays.csv
+if dt.datetime.today().month == birthday.month.values:
+    if dt.datetime.today().day == birthday.day.values:
+        print("Happy birthday!")
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
