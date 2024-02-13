240ac83f409bd0f7b815bf913f692786f0275db0
Chillpill9797 <adityagoswami193@gmail.com>
2024-02-13 23:15:36 +0530

    apending the name in the letter

--- Birthday-wisher/main.py
+++ Birthday-wisher/main.py
@@ -5,10 +5,17 @@ import random
 import pandas as pd
 
 birthday = pd.read_csv("birthdays.csv")
+today_month = dt.datetime.today().month
+today_day = dt.datetime.today().day
 # 2. Check if today matches a birthday in the birthdays.csv
-if dt.datetime.today().month == birthday.month.values:
-    if dt.datetime.today().day == birthday.day.values:
-        print("Happy birthday!")
+for index, row in birthday.iterrows():
+    name = row["name"]
+    if today_month == row["month"] and today_day == row["day"]:
+        with open("letter_templates/letter_1.txt") as body:
+            content_read = body.read()
+            content_edit = content_read.replace("[NAME]", name)
+            print(content_edit)
+
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
