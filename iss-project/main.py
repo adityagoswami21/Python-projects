eddb05edebd0c9ec4b5c5ceb0323a0d017c0185c
Chillpill9797 <adityagoswami193@gmail.com>
2024-03-03 15:51:24 +0530

    update

---
+++ iss-project/main.py
@@ -0,0 +1,32 @@
+import requests
+from datetime import datetime
+MY_LAT = 20.593683
+MY_LONG = 78.962883
+
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    longitude = float(data["iss_position"]["longitude"])
+    latitude = float(data["iss_position"]["latitude"])
+    if MY_LAT-5 <= latitude <= MY_LAT+5 and MY_LONG-5 <= longitude <= MY_LONG+5:
+        return True
+
+
+def is_night():
+    parameter = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameter)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now()
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
