2b6b952e6f6a2e284dc381c8a612ae6973ae0e5b
Chillpill9797 <adityagoswami193@gmail.com>
2024-05-10 22:26:48 +0530

    getting access to all item-ids in store

--- Cookie-clicker-Selenium/main.py
+++ Cookie-clicker-Selenium/main.py
@@ -7,4 +7,7 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, "cookie")
-
+five_sec = time.time() + 5
+five_min = time.time() + 60*5
+store = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+items_id = [item.get_attribute("id") for item in store]
