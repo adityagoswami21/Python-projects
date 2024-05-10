9ef8f98ce2ed21ff9874aef114cc6b902f3bfc91
Chillpill9797 <adityagoswami193@gmail.com>
2024-05-11 00:26:15 +0530

    getting access to price of each item

--- Cookie-clicker-Selenium/main.py
+++ Cookie-clicker-Selenium/main.py
@@ -9,7 +9,7 @@ driver.get("https://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, "cookie")
 five_sec = time.time() + 5
 five_min = time.time() + 60*5
-store = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+store = driver.find_elements(By.CSS_SELECTOR, value="#store div b")
 items_id = [item.get_attribute("id") for item in store]
 
 while True:
@@ -17,6 +17,11 @@ while True:
     if time.time() > five_sec:
         score = driver.find_element(By.ID, value="money")
         int_score = int(score.text)
-        print(int_score)
+        # print(int_score)
+
+        affordable_upgrades = []
+        for upgrade in store:
+            upgrade_price = upgrade.text.split("-")[1].strip().replace(",","")
+            print(upgrade_price)
         break
 
