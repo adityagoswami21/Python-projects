907502ed7351e28adb6caba6aa397a2017ecca57
Chillpill9797 <adityagoswami193@gmail.com>
2024-05-12 01:03:36 +0530

    cookie_upgrade dictionary and some small changes

--- Cookie-clicker-Selenium/main.py
+++ Cookie-clicker-Selenium/main.py
@@ -9,21 +9,29 @@ driver.get("https://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, "cookie")
 five_sec = time.time() + 5
 five_min = time.time() + 60*5
-store = driver.find_elements(By.CSS_SELECTOR, value="#store div b")[:-1]
+store = driver.find_elements(By.CSS_SELECTOR, value="#store div")[:-1]
 items_id = [item.get_attribute("id") for item in store]
+print(items_id)
 while True:
     cookie.click()
     if time.time() > five_sec:
-        score = driver.find_element(By.ID, value="money")
-        int_score = int(score.text)
+        score = driver.find_element(By.ID, value="money").text
+        if "," in score:
+            score = score.replace(",", "")
+        int_score = int(score)
         # print(int_score)
 
-        affordable_upgrades = []
-        for upgrade in store:
+        cost_of_upgrades = []
+        for upgrade in driver.find_elements(By.CSS_SELECTOR, value="#store div b")[:-1]:
             upgrade_text = upgrade.text
             upgrade_price = upgrade_text.split("-")[1].strip().replace(",", "")
             print(upgrade_price)
-            affordable_upgrades.append(int(upgrade_price))
-        print(affordable_upgrades)
-        break
+            cost_of_upgrades.append(upgrade_price)
+        print(cost_of_upgrades)
+
+        cookie_upgrades = {}
+        for n in range(len(cost_of_upgrades)):
+            cookie_upgrades[cost_of_upgrades[n]] = items_id[n]
+
+
 
