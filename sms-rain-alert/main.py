124e77c12e4e9b5e91da495f488869e629a60e83
Chillpill9797 <adityagoswami193@gmail.com>
2024-05-21 00:01:15 +0530

    console functioning done

--- sms-rain-alert/main.py
+++ sms-rain-alert/main.py
@@ -7,4 +7,14 @@ params = {
 }
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=params)
-
+weather_data = response.json()
+will_rain = False
+weather_ids = []
+for ids in weather_data['list']:
+    weather_id = ids['weather'][0]['id']
+    # print(f"id: {weather_id}")
+    weather_ids.append(int(weather_id))
+    if int(weather_id) < 700:
+        will_rain = True
+if will_rain:
+    print("Bring an umbrella!")
