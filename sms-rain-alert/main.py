c16048c6669bbd84ab38213db37572a5016f3856
Chillpill9797 <adityagoswami193@gmail.com>
2024-05-22 01:26:57 +0530

    accessing key from .env

--- sms-rain-alert/main.py
+++ sms-rain-alert/main.py
@@ -1,18 +1,18 @@
+from twilio.rest import Client
 import requests
 import os
-# from dotenv import load_dotenv
-# load_dotenv
-from creds import api_key
+from dotenv import load_dotenv
+load_dotenv()
 params = {
     "lat": 13.756331,
     "lon": 100.501762,
-    "appid": api_key,
+    "appid": os.getenv('api_key'),
     "cnt": 4
 }
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=params)
 weather_data = response.json()
-print(weather_data)
+# print(weather_data)
 will_rain = False
 weather_ids = []
 for ids in weather_data['list']:
