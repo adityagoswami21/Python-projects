9537cf0b495f3b49b4b8f0287da119e3d7b8a9dd
Chillpill9797 <adityagoswami193@gmail.com>
2024-05-22 01:37:31 +0530

    acc_sid and auth_token accessing

--- sms-rain-alert/main.py
+++ sms-rain-alert/main.py
@@ -2,6 +2,8 @@ from twilio.rest import Client
 import requests
 import os
 from dotenv import load_dotenv
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
 load_dotenv()
 params = {
     "lat": 13.756331,
