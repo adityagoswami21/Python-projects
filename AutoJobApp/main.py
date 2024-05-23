1c15ca52b26c17d4d8dadba7dfed0a6c0ce6c31d
Chillpill9797 <adityagoswami193@gmail.com>
2024-05-24 00:21:54 +0530

    auto login

--- AutoJobApp/main.py
+++ AutoJobApp/main.py
@@ -1,13 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import os
+from dotenv import load_dotenv
 import time
-
+load_dotenv()
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3924223523&distance=25&f_AL=true&geoId=102257491"
            "&keywords=python%20developer&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true")
+# Clicking Sign in button
 driver.find_element(By.XPATH, "/html/body/div[1]/header/nav/div/a[2]").click()
 
-
-
+time.sleep(10)
+#Email
+email_box = driver.find_element(By.XPATH, value="//*[@id='username']")
+email_box.send_keys(os.getenv("EMAIL"), Keys.ENTER)
+#Password
+password_box = driver.find_element(By.XPATH, value="//*[@id='password']")
+password_box.send_keys(os.getenv("PASSWORD"), Keys.ENTER)
