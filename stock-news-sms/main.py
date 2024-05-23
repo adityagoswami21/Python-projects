12b09641afaa008e7145d5598d2fca8b469c5768
Chillpill9797 <adityagoswami193@gmail.com>
2024-05-23 18:51:55 +0530

    accessing json format of stocks

--- stock-news-sms/main.py
+++ stock-news-sms/main.py
@@ -1,3 +1,8 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
@@ -9,7 +14,14 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 #HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
 #HINT 2: Work out the value of 5% of yerstday's closing stock price. 
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol":STOCK,
+    "apikey": os.getenv("STOCK_API")
 
+}
+response = requests.get(STOCK_ENDPOINT, stock_params)
+print(response.json())
 
 
 
