e68c20409c712214254e6f4c9d2fddb22b1380de
Chillpill9797 <adityagoswami193@gmail.com>
2025-07-23 21:30:26 +0530

    quiz game error solved

--- Quiz game/main.py
+++ Quiz game/main.py
@@ -2,9 +2,9 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 question_bank = []
-for question in question_data:
-    question_text = question['results'[3]]
-    question_answer = question['results'[4]]
+for question in question_data['results']:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
