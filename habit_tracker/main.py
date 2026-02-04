7b369d5d4f18e92fc4d92a259cf2481ec6bfe248
Chillpill9797 <adityagoswami193@gmail.com>
2026-02-04 16:55:52 +0530

    new project habit_tracker add, post request

---
+++ habit_tracker/main.py
@@ -0,0 +1,19 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+USERNAME = os.getenv('USERNAME')
+TOKEN = os.getenv("TOKEN")
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
